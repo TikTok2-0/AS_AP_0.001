bf55c22ac4e903bbc0a651358a33a53d605be4f2
eatenWatermelon <63154387+eatenWatermelon@users.noreply.github.com>
2021-01-15 21:51:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/ContactsViewAdapter.java
@@ -0,0 +1,95 @@
+package com.example.recyclerview;
+
+import android.content.Context;
+import android.graphics.BlendMode;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class ContactsViewAdapter extends RecyclerView.Adapter<ContactsViewAdapter.ViewHolder>{
+
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    private Context context;
+
+    public ContactsViewAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //                                                                                        wodran soll view attached sein, false weil true reduntant null wenn man sich nicht sicher ist wodran view attached ist
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contacts_list_item, parent, false);
+
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txtName.setText(contacts.get(position).getName());       //hier kann mehr hinzugefügt werden
+        //holder.txtEmail.setText(contacts.get(position).getEmail());
+
+        if(holder.txtName.getText().toString().contains("Moodle")){
+            holder.txtName.setTextColor(context.getResources().getColor(R.color.black));
+            holder.constraintLayout.setBackgroundColor(context.getResources().getColor(R.color.white));
+            holder.image.setAlpha(255);
+        }
+
+
+        holder.parent.setOnClickListener(new View.OnClickListener(){;
+            @Override
+            public void onClick(View v){
+                Toast.makeText(context, contacts.get(position).getName()+ " Selected",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //loading images
+        Glide.with(context)
+                .asBitmap()
+                .load(contacts.get(position).getImageUrl())
+                .into(holder.image);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    public void setContacts(ArrayList<Contact> contacts) {
+        this.contacts = contacts;
+        notifyDataSetChanged(); //wichtig damit die Daten sich aktualisieren
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView txtName; //txtEmail;
+        private CardView parent;
+        private ImageView image;
+        private ConstraintLayout constraintLayout;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtName = itemView.findViewById(R.id.txtName);
+            parent = itemView.findViewById(R.id.parent);
+            constraintLayout = itemView.findViewById(R.id.parentContraint);
+            //txtEmail = itemView.findViewById(R.id.txtEmail);
+
+            image = itemView.findViewById(R.id.image);
+        }
+    }
+}
