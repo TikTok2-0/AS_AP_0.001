8e8fbc4cec6ae48043b551647e102c0808c78c3b
mrdb12 <dbruchwald@gmail.com>
2021-05-08 19:59:37 +0200

    Working on BottomSheet Layout
    Signed-off-by: mrdb12 <dbruchwald@gmail.com>

--- app/src/main/java/com/example/recyclerview/AbirechnerBottomSheetDialog.java
+++ app/src/main/java/com/example/recyclerview/AbirechnerBottomSheetDialog.java
@@ -1,4 +1,43 @@
 package com.example.recyclerview;
 
-public class AbirechnerBottomSheetDialog {
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.viewpager2.widget.ViewPager2;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.relex.circleindicator.CircleIndicator3;
+
+
+public class AbirechnerBottomSheetDialog extends BottomSheetDialogFragment {
+
+    public static AbirechnerBottomSheetDialog newInstance() {
+        return new AbirechnerBottomSheetDialog();
+    }
+
+
+    Context context;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        context = getActivity();
+
+        View v = inflater.inflate(R.layout.bottomsheet_studenplan, container, false);  //stundenplan_bottom_sheet
+
+
+
+        return v;
+
+    }
+
 }
