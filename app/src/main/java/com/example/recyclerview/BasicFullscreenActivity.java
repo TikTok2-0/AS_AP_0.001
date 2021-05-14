7022e765b0ee9c972b06c9db218bb74384d7cec4
mrdb12 <dbruchwald@gmail.com>
2021-05-14 15:52:11 +0200

    merged master into development

---
+++ app/src/main/java/com/example/recyclerview/BasicFullscreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BasicFullscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_fullscreen);
+    }
+}
